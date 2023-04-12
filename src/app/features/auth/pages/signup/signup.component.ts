3d5390f246468b3f27ecb8028fad0a31b1f9a2be
Danai Munjemu <danaimunjemu@Danais-MacBook-Pro.local>
2023-04-12 08:37:22 +0200

    Sign Up Page

--- src/app/features/auth/pages/signup/signup.component.ts
+++ src/app/features/auth/pages/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {NotificationService} from "../../../../core/services/notification.service";
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+
+  constructor(
+    private router: Router,
+    private notificationService: NotificationService
+  ) {
+  }
+  signup(){
+    this.notificationService.createNotification({
+      type: 'success',
+      title: 'Sign Successful',
+      content: 'You can start planting your oaknuts!'
+    });
+    setTimeout(()=> this.router.navigateByUrl('/auth/login'), 1000);
+  }
 
 }
